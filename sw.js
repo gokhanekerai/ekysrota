b4b1444da0a20eb1ab3b2969769b1992aaf53024
gokhanekerai <gokhaneker@hotmail.com>
2026-08-30 02:20:22 +0300

    feat: Ergonomik quiz yerlesimi - 2 sutunlu siklar, butonlar altta, cizelge en altta (v6.1)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // EKYS 2027 Service Worker (Network First Strategy - Canlı Otomatik Güncelleme)
-const CACHE_NAME = 'ekys-2027-v6.0';
+const CACHE_NAME = 'ekys-2027-v6.1';
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();
