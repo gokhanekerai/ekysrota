a2adbcc3fa17b7ce789998d8f453b21aa99a284c
gokhanekerai <gokhaneker@hotmail.com>
2026-09-12 23:25:07 +0300

    fix(mobile): basari analizi kartlarinda mobilde tasma ve sigmama sorununu gider (v117.0)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // EKYSrota Service Worker (Network First Strategy - Canlı Otomatik Güncelleme)
-const CACHE_NAME = 'ekysrota-v116.0';
+const CACHE_NAME = 'ekysrota-v117.0';
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();
